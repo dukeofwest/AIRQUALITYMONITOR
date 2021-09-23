68b2aa2ee9e3e28768ec9c73a92fc86292513dee
Duke Ovie <dukeovie09@gmail.com>
2021-09-24 00:38:52 +0200

    Created frontendUI, header, and registration page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountriesComponent } from './countries/countries.component';
+import { DetailsComponent } from './details/details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'countries',
+    component: CountriesComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    redirectTo: '/countries',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
