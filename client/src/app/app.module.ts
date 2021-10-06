0e5200538e201fe8376a476c5ef2a642e70a80d9
Duke Ovie <dukeovie09@gmail.com>
2021-10-06 22:52:29 +0200

    pwa added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { CountriesService } from './countries.service';
 import { AuthGuard } from './auth.guard';
 import { TokenInterceptorService } from "./token-interceptor.service";
 import { MatomoModule } from "ngx-matomo";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -39,6 +41,12 @@ import { MatomoModule } from "ngx-matomo";
       routeTracking: {
         enable: true
       }
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   providers: [
